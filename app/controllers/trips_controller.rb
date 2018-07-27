7b05a7bba9cb09ac1a6fbc126c6c64cb209f03ac
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-27 10:22:51 -0600

    complete user story 1

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,4 +3,14 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name)
+  end
 end
--- spec/features/trip_index_spec.rb
+++ spec/features/trip_index_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
-# As a visitor,
-# when I visit the hiking trips index page,
-# I see a list of all hiking trip names in
-# the database.
-# As a visitor,
-# when I visit the hiking trips index page,
-# and I click on the name of a hiking trip,
-# I see a unique page for that hiking trip.
-
-
 describe "user sees all trips" do
   describe "they visit /trips" do
     it "displays all trips" do
       trip_1 = Trip.create(name: "Title 1")
       trip_2 = Trip.create(name: "Title 2")
 
-      visit '/trips'
+      visit trips_path
       expect(page).to have_content("All Trips")
       expect(page).to have_content(trip_1.name)
       expect(page).to have_content(trip_2.name)
+    end
+
+    it "click on name to see trip show page" do
+      trip_1 = Trip.create(name: "Title 1")
+
+      visit trips_path
+      click_link 'Title 1'
+
+      expect(current_path).to eq(trip_path(trip_1))
     end
   end
 end
