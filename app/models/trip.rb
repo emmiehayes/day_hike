26914b2334eaf94e6520094a243c3de72ddeb1b5
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-27 10:52:08 -0600

    total distance

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,10 @@
 class Trip < ApplicationRecord
 validates_presence_of :name
 
-  has_many :trip_trails, dependent: :destroy
-  has_many :trails, through: :trip_trails
+has_many :trip_trails, dependent: :destroy
+has_many :trails, through: :trip_trails
+
+  def total_length
+    trails.sum(:length)
+  end
 end
--- spec/features/trip_show_spec.rb
+++ spec/features/trip_show_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
 # As a visitor,
 # when I visit a hiking trip's page,
-# I see a list of trails included in the trip,
-# I see the name, address, and length for each trail.
+# I see the total hiking distance
+# of all trails on that hiking trip
+
+# As a visitor,
+# when I visit a hiking trip's page,
+# I see the average hiking distance
+# of all trails on that hiking trip
+
+# As a visitor,
+# when I visit a hiking trip's page,
+# I see the longest hiking distance
+# from all trails on that hiking trip
+
+# As a visitor,
+# when I visit a hiking trip's page,
+# I see the shortest hiking distance
+# from all trails on that hiking trip
 
 describe "user sees all trip" do
   describe "they visit /trip/1" do
@@ -22,14 +37,18 @@ describe "user sees all trip" do
       expect(page).to have_content(trail_2.length)
     end
 
-    it "see trail attributes for each trail" do
+    it "see total hiking distance for all trails" do
       trip_1 = Trip.create(name: "Title 1")
-      trip_2 = Trip.create(name: "Title 2")
+      trail_1 = trip_1.trails.create(name: 'trail_1', address: 'trail_1 address', length: 1)
+      trail_2 = trip_1.trails.create(name: 'trail_2', address: 'trail_2 address', length: 2)
 
-      visit trips_path
-      expect(page).to have_content("All Trips")
-      expect(page).to have_content(trip_1.name)
-      expect(page).to have_content(trip_2.name)
+      visit trip_path(trip_1)
+
+      expect(page).to have_content("Total Distance: #{trip_1.total_length}")
+
     end
+
+
+
   end
 end
